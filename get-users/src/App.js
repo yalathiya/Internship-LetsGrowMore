a008f811fbcc6d41a94cb6600c8bb5a97f6ec08c
yalathiya <yashlathiya59ppsv@gmail.com>
2022-11-04 09:49:35 +0530

    Project initialization

---
+++ get-users/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <>VARIYA GARMENT</>
+      <br></br>
+      <>GET USERS BUTTON</>
+      <br></br>
+      <>Data is fetched by api</>
+    </>
+  );
+}
+
+export default App;
