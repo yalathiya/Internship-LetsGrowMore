7cffcefff1ad53d4c2c742584f0eec9f4c2a10d6
yalathiya <yashlathiya59ppsv@gmail.com>
2022-11-26 17:22:37 +0530

    UI Designing of Calculator

---
+++ calculator/src/components/button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = (e) => {
+  return <input type="button" value={e.label} />;
+};
+
+export default Button;
---
+++ calculator/src/components/calculatorTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CalculatorTitle = (e) => {
+  return <div className="calculator-title">{e.value}</div>;
+};
+
+export default CalculatorTitle;
