045f5189fc326fd37d2b9b1602527f4ff4357c99
yalathiya <yashlathiya59ppsv@gmail.com>
2022-11-26 18:47:00 +0530

    necessary changes

--- calculator/src/components/button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = (e) => {
-  return <input type="button" value={e.label} />;
-};
-
-export default Button;
--- calculator/src/components/calculatorTitle.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CalculatorTitle = (e) => {
-  return <div className="calculator-title">{e.value}</div>;
-};
-
-export default CalculatorTitle;
