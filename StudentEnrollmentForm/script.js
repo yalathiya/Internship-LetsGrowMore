1e754d42c7c8d12270bd78ae217c3b5da15d465b
yalathiya <yashlathiya59ppsv@gmail.com>
2022-11-29 10:48:07 +0530

    Changes Done

--- StudentEnrollmentForm/script.js
+++ StudentEnrollmentForm/script.js
@@ -1,6 +1,7 @@
-let submit = document.querySelector("#submit-button");
-submit.onClick = myFunction();
-myFunction = () => {
-  document.getElementById("student-name").innerHTML =
-    document.getElementById("name-input").value;
+const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+submitButton.onClick = () => {
+  document.getElementById("#studentColumn").appendChild(para);
 };
