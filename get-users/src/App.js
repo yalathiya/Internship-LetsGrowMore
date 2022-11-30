3b2c9412765eb2c7aad8cd8680c0e4aff69217a7
yalathiya <yashlathiya59ppsv@gmail.com>
2022-11-30 12:21:48 +0530

    JS is done

--- get-users/src/App.js
+++ get-users/src/App.js
@@ -1,24 +1,38 @@
+import React, { useState } from "react";
 import "./App.css";
-import API from "./components/API.js";
-import CallAPI from "./components/CallAPI.js";
+// import Navbarr from "./components/Navbarr.js";
+// import API from "./components/API.js";
+// import CallAPI from "./components/CallAPI.js";
 
-function App() {
-  function sayHello() {
-    alert("Hello");
-  }
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async () => {
+    const response = await fetch("https://api.github.com/users");
+    const jsonResponse = await response.json();
+    setUsers(jsonResponse);
+  };
   return (
     <>
-      {/* <Navbar /> */}
+      {/* <Navbarr /> */}
       <>VARIYA GARMENT</>
       <br></br>
-      <>GET USERS BUTTON</>
       <br></br>
-      <button onClick={sayHello}>Get Users</button>
+      <button onClick={loadUsers}>Get Users</button>
       <br></br>
-      <CallAPI />
-      <>Data is fetched by api</>
+      {/* <CallAPI /> */}
+      {/* <>Data is fetched by api</> */}
+      <ul>
+        {users.map(({ id, login, url }) => (
+          <li key={id}>
+            ID: {id} <br></br>
+            Username : {login} <br></br>
+            Website : {url} <br></br>
+          </li>
+        ))}
+      </ul>
     </>
   );
-}
+};
 
 export default App;
